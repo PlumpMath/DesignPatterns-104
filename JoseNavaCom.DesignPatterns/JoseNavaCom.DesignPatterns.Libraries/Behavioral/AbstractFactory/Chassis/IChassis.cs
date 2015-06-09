187bfe66e24a3042007cfbccefdafe4733099d0b
josenavacom <jose@josenava.com>
2015-06-09 14:53:14 -0500

    Removed all the unused Using

--- JoseNavaCom.DesignPatterns/JoseNavaCom.DesignPatterns.Libraries/Behavioral/AbstractFactory/Chassis/IChassis.cs
+++ JoseNavaCom.DesignPatterns/JoseNavaCom.DesignPatterns.Libraries/Behavioral/AbstractFactory/Chassis/IChassis.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace JoseNavaCom.DesignPatterns.Libraries.Behavioral.AbstractFactory.Chassis
+﻿namespace JoseNavaCom.DesignPatterns.Libraries.Behavioral.AbstractFactory.Chassis
 {
     public interface IChassis
     {
