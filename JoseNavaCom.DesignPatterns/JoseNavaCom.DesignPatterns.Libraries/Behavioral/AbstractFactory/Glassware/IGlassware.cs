9ddf8a60a9b86eb66c4c020b41109f6d0e3a62d5
josenavacom <jose@josenava.com>
2015-06-09 14:54:23 -0500

    Made the Interface public and added the GlasswareParts string. Also removed unused Using

--- JoseNavaCom.DesignPatterns/JoseNavaCom.DesignPatterns.Libraries/Behavioral/AbstractFactory/Glassware/IGlassware.cs
+++ JoseNavaCom.DesignPatterns/JoseNavaCom.DesignPatterns.Libraries/Behavioral/AbstractFactory/Glassware/IGlassware.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace JoseNavaCom.DesignPatterns.Libraries.Behavioral.AbstractFactory.Glassware
 {
-    interface IGlassware
+    public interface IGlassware
     {
+        string GlasswareParts { get; }
     }
 }
