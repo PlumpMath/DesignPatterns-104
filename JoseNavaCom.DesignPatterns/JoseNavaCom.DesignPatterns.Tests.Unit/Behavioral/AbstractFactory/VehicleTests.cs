fd0a36a7fc16113a7b21671529b9b4773af02293
josenavacom <jose@josenava.com>
2015-06-09 20:28:26 -0500

    Unit Tests for the Abstract Factory Pattern

---
+++ JoseNavaCom.DesignPatterns/JoseNavaCom.DesignPatterns.Tests.Unit/Behavioral/AbstractFactory/VehicleTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JoseNavaCom.DesignPatterns.Libraries.Behavioral.AbstractFactory;
+using JoseNavaCom.DesignPatterns.Libraries.Behavioral.AbstractFactory.VehicleFactory;
+using JoseNavaCom.DesignPatterns.Libraries.Behavioral.AbstractFactory.Chassis;
+
+namespace JoseNavaCom.DesignPatterns.Tests.Unit.Behavioral.AbstractFactory
+{
+    [TestClass]
+    public class VehicleTests
+    {
+
+        [TestMethod]
+        public void Get_Returns_Car_Factory()
+        {
+            Vehicle vehicle = new Vehicle();
+            AbstractVehicleFactory vehicleFactory = vehicle.Get(VehicleType.Car);
+            Assert.IsInstanceOfType(vehicleFactory, typeof(CarFactory));
+        }
+
+        [TestMethod]
+        public void Get_Returns_Body_Shell_For_Car()
+        {
+            Vehicle vehicle = new Vehicle();
+            AbstractVehicleFactory vehicleFactory = vehicle.Get(VehicleType.Car);
+            IChassis vehicleChassis = vehicleFactory.CreateChassis();
+            Assert.AreEqual(vehicleChassis.ChassisParts, "Chassis parts for a car");
+        }
+    }
+}
