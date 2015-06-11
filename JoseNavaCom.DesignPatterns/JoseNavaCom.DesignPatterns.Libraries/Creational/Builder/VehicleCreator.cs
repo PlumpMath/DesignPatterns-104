dec50e576f0b629015b1a3104dcc8a355a82a0ce
josenavacom <jose@josenava.com>
2015-06-10 20:15:20 -0500

    Removed unused Using

--- JoseNavaCom.DesignPatterns/JoseNavaCom.DesignPatterns.Libraries/Creational/Builder/VehicleCreator.cs
+++ JoseNavaCom.DesignPatterns/JoseNavaCom.DesignPatterns.Libraries/Creational/Builder/VehicleCreator.cs
@@ -1,9 +1,5 @@
 ﻿using JoseNavaCom.DesignPatterns.Libraries.Creational.Builder.VehicleBuilder;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace JoseNavaCom.DesignPatterns.Libraries.Creational.Builder
 {
