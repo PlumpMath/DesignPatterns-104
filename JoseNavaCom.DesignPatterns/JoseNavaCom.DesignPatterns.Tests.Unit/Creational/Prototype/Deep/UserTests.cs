ca16d7e21a2d77eb6ee1a17ae76b75648cca3692
josenavacom <jose@josenava.com>
2015-06-12 21:09:28 -0500

    Unit Tests for Prototype Pattern

---
+++ JoseNavaCom.DesignPatterns/JoseNavaCom.DesignPatterns.Tests.Unit/Creational/Prototype/Deep/UserTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JoseNavaCom.DesignPatterns.Libraries.Creational.Prototype.Deep;
+
+namespace JoseNavaCom.DesignPatterns.Tests.Unit.Creational.Prototype.Deep
+{
+    [TestClass]
+    public class UserTests
+    {
+        [TestMethod]
+        public void User_Returns_Correct_Role_Name()
+        {
+            Role role = new Role();
+            role.Id = 1;
+            role.Name = "Administrator";
+
+            User user = new User(999);
+            user.Role = role;
+            User userClone = (User)user.Clone();
+            Assert.AreEqual<string>("Administrator", userClone.Role.Name);
+        }
+    }
+}
---
+++ JoseNavaCom.DesignPatterns/JoseNavaCom.DesignPatterns.Tests.Unit/Creational/Prototype/Shallow/UserTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JoseNavaCom.DesignPatterns.Libraries.Creational.Prototype.Shallow;
+
+namespace JoseNavaCom.DesignPatterns.Tests.Unit.Creational.Prototype.Shallow
+{
+    [TestClass]
+    public class UserTests
+    {
+        [TestMethod]
+        public void User_Returns_Correct_Id()
+        {
+            User user = new User(999);
+            User userClone = (User)user.Clone();
+            Assert.AreEqual<int>(999, userClone.Id);
+        }
+    }
+}
