2a9b89f0539218f638506466ae63770631cdf2c5
josenavacom <jose@josenava.com>
2015-06-12 21:09:05 -0500

    Prototype Pattern

---
+++ JoseNavaCom.DesignPatterns/JoseNavaCom.DesignPatterns.Libraries/Creational/Prototype/Cloneable/User.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace JoseNavaCom.DesignPatterns.Libraries.Creational.Prototype.Cloneable
+{
+    public class User : ICloneable
+    {
+        private int _id;
+
+        public User(int id)
+        {
+            _id = id;
+        }
+
+        public int Id
+        {
+            get
+            {
+                return _id;
+            }
+        }
+
+        public Role Role { get; set; }
+
+
+        private object ShallowCopy()
+        {
+            return this.MemberwiseClone();
+        }
+
+        private object DeepCopy()
+        {
+            User user = (User)this.MemberwiseClone();
+            user.Role = new Role();
+            user.Role.Id = this.Role.Id;
+            user.Role.Name = this.Role.Name;
+
+            return user;
+        }
+
+        public object Clone()
+        {
+            return DeepCopy();
+        }
+
+    }
+}
