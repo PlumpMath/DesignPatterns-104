90d69390f171de9030ae8818c10b59e9fd0d17fc
josenavacom <jose@josenava.com>
2015-06-14 23:00:56 -0500

    Unit Tests for the Adapter Pattern

---
+++ JoseNavaCom.DesignPatterns/JoseNavaCom.DesignPatterns.Tests.Unit/Structural/Adapter/ClientTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JoseNavaCom.DesignPatterns.Libraries.Structural.Adapter;
+using Moq;
+
+namespace JoseNavaCom.DesignPatterns.Tests.Unit.Structural.Adapter
+{
+    [TestClass]
+    public class ClientTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(System.ArgumentNullException))]
+        public void Contructor_Throws_Argument_Null_Exception()
+        {
+            Client vehicle = new Client(null);
+        }
+
+        [TestMethod]
+        public void MakeRequest_Returns_MethodB()
+        {
+            string expected = "MethodB called";
+
+            Mock<ITarget> _mockTarget = new Mock<ITarget>();
+            _mockTarget.Setup(m => m.GetMethodA()).Returns("MethodB called");
+
+            Client vehicle = new Client(_mockTarget.Object);
+            string result = vehicle.MakeRequest();
+            Assert.AreEqual<string>(expected, result);
+        }
+    }
+}
