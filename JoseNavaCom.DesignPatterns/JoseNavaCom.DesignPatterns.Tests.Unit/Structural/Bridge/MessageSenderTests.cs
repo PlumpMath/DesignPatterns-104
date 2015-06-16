6a058320c3e771de9cef1e5cdff7a3adaac349de
josenavacom <jose@josenava.com>
2015-06-15 22:09:18 -0500

    Unit Tests for the Bridge Pattern

---
+++ JoseNavaCom.DesignPatterns/JoseNavaCom.DesignPatterns.Tests.Unit/Structural/Bridge/MessageSenderTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JoseNavaCom.DesignPatterns.Libraries.Structural.Bridge;
+using JoseNavaCom.DesignPatterns.Libraries.Structural.Bridge.Message;
+
+namespace JoseNavaCom.DesignPatterns.Tests.Unit.Structural.Bridge
+{
+    [TestClass]
+    public class MessageSenderTests
+    {
+        [TestMethod]
+        public void Send_Returns_SystemMessage()
+        {
+            MessageSender messageSender = new MessageSender();
+            Message senderMessage = messageSender.Send();
+
+            Assert.AreEqual<string>("Test Message", senderMessage.Subject);
+        }
+    }
+}
