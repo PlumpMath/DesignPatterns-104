f58cdfa5b885248c145f3a02200916ce382b2193
josenavacom <jose@josenava.com>
2015-06-19 00:13:48 -0500

    Unit Tests for the Decorator Design Pattern

---
+++ JoseNavaCom.DesignPatterns/JoseNavaCom.DesignPatterns.Tests.Unit/Structural/Decorator/CakeDecoratorTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JoseNavaCom.DesignPatterns.Libraries.Structural.Decorator;
+
+namespace JoseNavaCom.DesignPatterns.Tests.Unit.Structural.Decorator
+{
+    [TestClass]
+    public class CakeDecoratorTests
+    {
+        [TestMethod]
+        public void GetPrice_Returns_210()
+        {
+            CakeDecorator cakeDecorator = new CakeDecorator();
+            var totalPrice = cakeDecorator.GetPrice();
+        }
+    }
+}
