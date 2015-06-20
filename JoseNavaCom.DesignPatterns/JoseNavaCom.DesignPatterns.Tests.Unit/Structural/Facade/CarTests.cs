a314e455db26a0c5a607c2f48bee79c6482701e5
josenavacom <jose@josenava.com>
2015-06-19 21:23:26 -0500

    Unit Tests for the Facade Pattern

---
+++ JoseNavaCom.DesignPatterns/JoseNavaCom.DesignPatterns.Tests.Unit/Structural/Facade/CarTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JoseNavaCom.DesignPatterns.Libraries.Structural.Facade;
+
+namespace JoseNavaCom.DesignPatterns.Tests.Unit.Structural.Facade
+{
+    [TestClass]
+    public class CarTests
+    {
+        [TestMethod]
+        public void Create_Returns_CarEngine()
+        {
+            Car car = new Car();
+            CarFacade carFacade = car.Create();
+            Assert.AreEqual<string>("CarEngine - SetEngine", carFacade.Engine);
+        }
+    }
+}
