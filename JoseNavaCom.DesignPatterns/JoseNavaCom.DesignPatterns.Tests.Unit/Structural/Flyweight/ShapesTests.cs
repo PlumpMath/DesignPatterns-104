e7695c593f8039772fa224fbadc0414eb448a229
josenavacom <jose@josenava.com>
2015-06-19 22:47:14 -0500

    Unit Tests for the Flyweight Pattern

---
+++ JoseNavaCom.DesignPatterns/JoseNavaCom.DesignPatterns.Tests.Unit/Structural/Flyweight/ShapesTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JoseNavaCom.DesignPatterns.Libraries.Structural.Flyweight;
+
+namespace JoseNavaCom.DesignPatterns.Tests.Unit.Structural.Flyweight
+{
+    [TestClass]
+    public class ShapesTests
+    {
+        [TestMethod]
+        public void GetFromName_Returns_Rectangle()
+        {
+            Shapes shapes = new Shapes();
+            IShape rectangle = shapes.GetFromName("Rectangle");
+            Assert.IsInstanceOfType(rectangle, typeof(Rectangle));
+        }
+
+        [TestMethod]
+        public void GetFromName_Returns_Priting_Rectangle()
+        {
+            Shapes shapes = new Shapes();
+            IShape rectangle = shapes.GetFromName("Rectangle");
+            Assert.AreEqual<string>("Printing Rectangle", rectangle.Print());
+        }
+    }
+}
